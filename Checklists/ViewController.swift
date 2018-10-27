5017f1aa63d21add2ae023b241588fa32832c8af
Kerem Balcı <kerembalci90@gmail.com>
2018-10-27 13:23:42 -0400

    Initial Commit

---
+++ Checklists/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Checklists
+//
+//  Created by Kerem BALCI on 2018-10-27.
+//  Copyright © 2018 Hextorm. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
